a8eb3c497439f50e6d69eddec8e0976f511ede68
Valentin Matton <taq@protonmail.ch>
2026-04-29 21:23:36 +0200

    feat: add timeline

--- make_gif.py
+++ make_gif.py
@@ -3,20 +3,176 @@
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
+#     "pendulum",
 #     "pillow",
+#     "plotnine",
+#     "polars",
+#     "pyarrow",
 # ]
 # ///
 
+import io
+import textwrap
 from pathlib import Path
-from PIL import Image
-from datetime import date
 from typing import TypedDict
+from functools import cache
+
+import pendulum
+import plotnine as gg
+import polars as pl
+from PIL import Image
 
 INPUT_DIR = Path(__file__).parent / "monde"
 OUTPUT_FILE = Path(__file__).parent / "animation.gif"
 
 
+EVENTS_DF = pl.DataFrame(
+    [
+        {"date": pendulum.Date(2020, 3, 11), "label": "Pandémie de COVID-19"},
+        {"date": pendulum.Date(2022, 2, 24), "label": "Invasion de l’Ukraine"},
+    ]
+).with_columns(
+    pl.col("label").map_elements(
+        lambda label: "\n".join(textwrap.wrap(label, width=15)),
+    )
+)
+
+
+@cache
+def make_timeline_base_plot(
+    range_: tuple[pendulum.Date, pendulum.Date],
+    color: str,
+):
+    data = (
+        pl.date_range(
+            start=range_[0],
+            end=range_[1],
+            interval="1y",
+            eager=True,
+        )
+        .alias("date")
+        .to_frame()
+    )
+    return (
+        gg.ggplot(data=data)
+        + gg.geom_rect(
+            data=data.select(
+                pl.col("date").alias("start"),
+                pl.col("date").shift(-1).alias("end"),
+            ).filter(pl.col("end").is_not_null()),
+            mapping=gg.aes(
+                xmin="start",
+                xmax="end",
+                ymin=0,
+                ymax=30,
+            ),
+            color=color,
+            fill=None,
+        )
+        + gg.geom_text(
+            data=data,
+            mapping=gg.aes(
+                x="date",
+                y=-30,
+                label="date.dt.year",
+            ),
+            size=6,
+            color=color,
+            va="baseline",
+        )
+        + gg.geom_segment(
+            data=EVENTS_DF,
+            mapping=gg.aes(
+                x="date",
+                xend="date",
+                y=40,
+                yend=100,
+            ),
+            color=color,
+        )
+        + gg.geom_text(
+            data=EVENTS_DF,
+            mapping=gg.aes(
+                x="date",
+                y=110,
+                label="label",
+            ),
+            position=gg.position_nudge(x=20),
+            color=color,
+            size=6,
+            ha="left",
+            va="top",
+        )
+        + gg.theme_void()
+    )
+
+
+def make_timeline(
+    range_: tuple[pendulum.Date, pendulum.Date],
+    highlight: tuple[pendulum.Date, pendulum.Date],
+    width_px: int,
+    height_px: int,
+    color: str,
+) -> Image:
+    plot = make_timeline_base_plot(
+        range_=range_,
+        color=color,
+    ) + gg.annotate(
+        "rect",
+        xmin=highlight[0],
+        xmax=highlight[1],
+        ymin=0,
+        ymax=30,
+        fill=color,
+        alpha=0.5,
+    )
+
+    dpi = 150
+
+    fig = plot.draw()
+    fig.set_size_inches(w=width_px / dpi, h=height_px / dpi, forward=False)
+
+    with io.BytesIO() as buf:
+        fig.savefig(
+            buf,
+            dpi=dpi,
+            transparent=True,
+        )
+        buf.seek(0)
+        image = Image.open(buf).convert("RGBA")
+        image.load()
+
+    return image
+
+
+def insert_timeline(
+    image: Image.Image,
+    range_: tuple[pendulum.DateTime, pendulum.DateTime],
+    highlight: tuple[pendulum.DateTime, pendulum.DateTime],
+) -> Image.Image:
+    background = image.copy()
+
+    timeline = make_timeline(
+        range_=range_,
+        highlight=highlight,
+        width_px=background.width // 2,
+        height_px=background.height // 10,
+        color="#{:02x}{:02x}{:02x}".format(*background.getpixel((1, 1))),
+    )
+
+    background.paste(
+        im=timeline,
+        box=(
+            int(0.35 * background.width),
+            int(0.80 * background.height),
+        ),
+        mask=timeline,
+    )
+    return background
+
+
 def normalize(image: Image.Image, size: tuple[int, int]) -> Image.Image:
+    image = image.copy()
     image = image.resize(size, resample=Image.Resampling.LANCZOS)
     image = image.convert("RGBA")
     return image
@@ -32,7 +188,7 @@ def fade(before: Image.Image, after: Image.Image, steps: int) -> list[Image.Imag
 
 
 class ImageDict(TypedDict):
-    timestamp: date
+    timestamp: pendulum.Date
     image: Image.Image
 
 
@@ -45,7 +201,7 @@ def main() -> None:
 
     images = [
         ImageDict(
-            timestamp=date.fromisoformat(path.stem[:8]),
+            timestamp=pendulum.Date.fromisoformat(path.stem[:8]),
             image=Image.open(path),
         )
         for path in sorted(INPUT_DIR.glob("*.jpg"))
@@ -56,7 +212,26 @@ def main() -> None:
 
     size = images[0]["image"].size
 
-    images = [{**image, "image": normalize(image["image"], size)} for image in images]
+    for image in images:
+        image["image"] = normalize(
+            image=image["image"],
+            size=size,
+        )
+
+    range_ = (
+        images[0]["timestamp"].start_of("year"),
+        images[-1]["timestamp"].start_of("year") + pendulum.Duration(years=1),
+    )
+    for image, next_image in zip(images, [*images[1:], None]):
+        if next_image is not None:
+            highlight = (image["timestamp"], next_image["timestamp"])
+        else:
+            highlight = (image["timestamp"], range_[1])
+        image["image"] = insert_timeline(
+            image=image["image"],
+            range_=range_,
+            highlight=highlight,
+        )
 
     black = Image.new("RGBA", size, (0, 0, 0, 0))
 
